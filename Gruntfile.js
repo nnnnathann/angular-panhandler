df214cd0780c368b6ecfb15f56cb719d3744c135
Nathan Bleigh <nathan.bleigh@gmail.com>
2013-12-03 09:42:55 -0600

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,95 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      },
+      example: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'example/js/<%= pkg.name %>.js'
+      },
+      dependencies: {
+        src: ['bower_components/angular/angular.js'],
+        dest: 'example/js/dependencies.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    connect: {
+      dev: {
+        options: {
+          port: 9001,
+          base: 'example'
+        }
+      }
+    },
+    open: {
+      dev : {
+        path: 'http://127.0.0.1:9001/'
+      }
+    },
+    watch: {
+      options: {
+        // livereload: true
+      },
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_concat: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'concat']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'connect','open','watch']);
+
+};
---
+++ example/examples.js
@@ -0,0 +1,16 @@
+/*globals angular */
+(function(){
+  'use strict';
+  angular.module('pannableExamples', ['panhandler'])
+    .controller('Example1', function Example1($scope) {
+      $scope.gridItems = generateGrid(30);
+    });
+
+  function generateGrid(count){
+    var items = [];
+    for(var i=0;i<count;i++){
+      items.push({ label: count });
+    }
+    return items;
+  }
+}());
